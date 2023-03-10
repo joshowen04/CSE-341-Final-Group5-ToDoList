4dc69c65be1ef5c201566e8edbc3f75a32a145d8
jaredvance26 <jaredkvance@gmail.com>
2023-03-10 12:56:19 -0700

    routes

--- routes/shoppingList.js
+++ routes/shoppingList.js
@@ -1,20 +1,47 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const shoppingListController = require('../controllers/shoppingListControllers');
+const shoppingListController = require("../controllers/shoppingListControllers");
+
+// // CREATE a new shopping list item
+// router.post('/', shoppingListController.create);
+
+// // READ all shopping list items
+// router.get('/', shoppingListController.getAll);
+
+// // READ a single shopping list item by ID
+// router.get('/:id', shoppingListController.getById);
+
+// // UPDATE a shopping list item by ID
+// router.patch('/:id', shoppingListController.updateById);
+
+// // DELETE a shopping list item by ID
+// router.delete('/:id', shoppingListController.deleteById);
+
+// module.exports = router;
 
 // CREATE a new shopping list item
-router.post('/', shoppingListController.create);
+router.post("/shopping-list", (req, res) => {
+  res.status(201).send("This works");
+});
 
 // READ all shopping list items
-router.get('/', shoppingListController.getAll);
+router.get("/shopping-list", (req, res) => {
+  res.status(201).send("This works");
+});
 
 // READ a single shopping list item by ID
-router.get('/:id', shoppingListController.getById);
+router.get("/shopping-list/:id", (req, res) => {
+  res.status(201).send("This works");
+});
 
 // UPDATE a shopping list item by ID
-router.patch('/:id', shoppingListController.updateById);
+router.put("/shopping-list/:id", (req, res) => {
+  res.status(201).send("This works");
+});
 
 // DELETE a shopping list item by ID
-router.delete('/:id', shoppingListController.deleteById);
+router.delete("/shopping-list/:id", (req, res) => {
+  res.status(201).send("This works");
+});
 
 module.exports = router;
