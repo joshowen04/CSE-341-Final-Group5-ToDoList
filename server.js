d6796bf1e4b2d04196066bd9ba7aaaa5a318a078
joshowen04 <owe21015@byui.edu>
2023-03-10 01:45:17 -0300

    added inventory to server.js

--- server.js
+++ server.js
@@ -1,20 +1,22 @@
 const path = require('path');
 const connectDB = require('./config/db');
-const express = require("express");
-const cors = require("cors");
-const swaggerUi = require("swagger-ui-express");
-const swaggerDocument = require("./swagger.json");
+const express = require('express');
+const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 const app = express();
 
-require("dotenv").config();
+require('dotenv').config();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json())
-    .use(cors())
-    .use("/user", require("./routes/userRoutes"))
-    .use("/shoppingList", require("./routes/shoppingList"))
-    .use("/todo", require("./routes/todo"))
-    .use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
-    .listen(PORT, function () {
-        console.log(`Listening on port ${PORT}`);
-    });
+app
+  .use(express.json())
+  .use(cors())
+  .use('/user', require('./routes/userRoutes'))
+  .use('/shoppingList', require('./routes/shoppingList'))
+  .use('/inventory', require('./routes/inventory'))
+  .use('/todo', require('./routes/todo'))
+  .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+  .listen(PORT, function () {
+    console.log(`Listening on port ${PORT}`);
+  });
