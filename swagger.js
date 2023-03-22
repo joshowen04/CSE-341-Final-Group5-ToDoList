4be91e914d062423d93ca25ce4ee792b5f9a5f12
jaredvance26 <jaredkvance@gmail.com>
2023-03-22 13:53:59 -0700

    inventory api calls

--- swagger.js
+++ swagger.js
@@ -3,10 +3,11 @@ const swaggerAutogen = require('swagger-autogen')();
 const doc = {
   info: {
     title: 'CSE-341-Final-Group5-ToDoList',
-    description: 'Group 5 final project, Advanced todo list',
+    description: 'Group 5 final project, Advanced todo list'
   },
   host: 'cse-341-final-group5-todolist.onrender.com',
-  schemes: ['https'],
+  //   host: 'localhost:3000',
+  schemes: ['https']
 };
 
 const outputFile = './swagger.json';
