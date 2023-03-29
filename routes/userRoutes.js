65fbc86913372c07ea9670f2fe21582807ed8ee2
joshowen04 <owe21015@byui.edu>
2023-03-28 21:10:53 -0300

    userroutes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,16 +1,45 @@
-'use strict';
-module.exports = function (app) {
-  const users = require('../controllers/userController');
-  const swaggerUi = require('swagger-ui-express');
-  const swaggerDocument = require('../swagger.json');
-  app.use(swaggerUi.serve);
+// 'use strict';
+// module.exports = function (app) {
+//   const users = require('../controllers/userController');
+//   const swaggerUi = require('swagger-ui-express');
+//   const swaggerDocument = require('../swagger.json');
+//   app.use(swaggerUi.serve);
 
-  app.route('/users').get(users.list_all_users).post(users.create_user);
-  app.route('/api-docs').get(swaggerUi.setup(swaggerDocument));
+//   app.route('/users').get(users.list_all_users).post(users.create_user);
+//   app.route('/api-docs').get(swaggerUi.setup(swaggerDocument));
 
-  app
-    .route('/users/:userId')
-    .get(users.read_user)
-    .put(users.update_user)
-    .delete(users.delete_user);
-};
+//   app
+//     .route('/users/:userId')
+//     .get(users.read_user)
+//     .put(users.update_user)
+//     .delete(users.delete_user);
+// };
+
+
+
+const express = require('express');
+const router = express.Router();
+const users = require('../controllers/userController');
+
+
+router.get('/users', (req, res) => {
+  users.list_all_users(req, res);
+});
+
+router.get('/users/:userId', (req, res) => {
+  users.read_user(req, res);
+});
+
+router.post('/users', (req, res) => {
+  users.create_user(req, res);
+});
+
+router.put('/users/:userId', (req, res) => {
+  users.update_user(req, res);
+});
+
+router.delete('/users/:userId', (req, res) => {
+  users.delete_user(req, res);
+});
+
+module.exports = router;
